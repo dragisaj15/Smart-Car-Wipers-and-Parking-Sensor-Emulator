bd12b18e92c8b7cce313cd74c1c2b1d709bad5e9
dragisaj15 <79987985+dragisaj15@users.noreply.github.com>
2026-03-06 00:10:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 /*********************************************************************
- * Projekat radili: Nikola Jevtic EE164/2019, Marija Bojic EE99/2019,
- * Milan Stanojevic EE100/2019 i Dragisa Jakisic EE32/2019.
- * Zadatak je bio da se emulira rad brisaca na automobilu preko servo
- * motora SG90. Sistem se pali slanjem ON sa terminala. Pri sobnom osve-
- * tljenju, servo motor ne radi - sija sunce. Kada se uperi u fotootpornik
- * blic, simuliramo pljusak, stoga brisaci rade najvecom brzinom. Ukoliko 
- * prekrijemo fotootpornik i vrednosti AD konverzije sa fotootpornika postanu
- * veoma male ~ 0010, kisa sipi. Prilikom konstantnog rada servo mo-
- * tora(kisa sipi i kisa pljusti), cela ploca je opterecena,
- * pogotovo zbog AD konverzije, pa u tom slucaju
- * ostali senzori ne rade, ali dok sija sunce, svi senzori rade normalno.
- * Datum odbrane projekta: 17.2.2023.
+ * Project Authors: Nikola Jevtic EE164/2019, Marija Bojic EE99/2019,
+ * Milan Stanojevic EE100/2019 and Dragisa Jakisic EE32/2019.
+ * Task: Emulate car wipers using an SG90 servo motor.
+ * The system is turned on by sending "ON" from the serial terminal.
+ * At room lighting (sunny), the servo motor is idle. 
+ * Shining a flashlight at the photoresistor simulates a heavy downpour,
+ * so the wipers run at maximum speed. If the photoresistor is covered
+ * and ADC values become very low (~0010), it simulates light rain.
+ * During constant servo operation (light or heavy rain), the board is 
+ * under heavy load (especially due to ADC conversion), so other sensors 
+ * are disabled. However, when it is sunny, all sensors operate normally.
+ * Project Defense Date: Feb 17, 2023.
 *********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<p30fxxxx.h>
+#include <p30fxxxx.h>
 #include "glcd_lib.h"
 #include "adc.h"
 
-
-_FOSC(CSW_FSCM_OFF & XT_PLL4);//instruction takt je isti kao i kristal 10MHz
-_FWDT(WDT_OFF);
-
+_FOSC(CSW_FSCM_OFF & XT_PLL4); // Instruction clock is the same as the 10MHz crystal
+_FWDT(WDT_OFF);                // Disable Watchdog Timer
 
 unsigned int sirovi0,sirovi1,sirovi2,sirovi3,sirovi4;
 unsigned int broj,broj1,broj2,tempRX, temp0, temp1, n, flag;
@@ -39,7 +37,6 @@ int buf[1];
 #define DRIVE_A PORTCbits.RC13
 #define DRIVE_B PORTCbits.RC14
 
-
 void ConfigureTSPins(void)
 {
 	//ADPCFGbits.PCFG10=1;
@@ -53,65 +50,61 @@ void ConfigureTSPins(void)
 	//LATCbits.LATC13=0;
 }
 
-//Delay za tocuh screen
+// Delay for touch screen
 void Delay(unsigned int N)
 {
 	unsigned int i;
 	for(i=0;i<N;i++);
 }
+
 void Touch_Panel (void)
 {
-// vode horizontalni tranzistori
+    // Horizontal transistors are conducting
 	DRIVE_A = 1;  
 	DRIVE_B = 0;
     
-     LATCbits.LATC13=1;
-     LATCbits.LATC14=0;
+    LATCbits.LATC13=1;
+    LATCbits.LATC14=0;
 
-	Delay(500); //cekamo jedno vreme da se odradi AD konverzija
+	Delay(500); // Wait for AD conversion to complete
 				
-	// ocitavamo x	
-	x_vrednost = temp0;//temp0 je vrednost koji nam daje AD konvertor na BOTTOM pinu		
+	// Read X coordinate	
+	x_vrednost = temp0; // temp0 is the AD converter value on the BOTTOM pin		
 
-	// vode vertikalni tranzistori
-     LATCbits.LATC13=0;
-     LATCbits.LATC14=1;
+	// Vertical transistors are conducting
+    LATCbits.LATC13=0;
+    LATCbits.LATC14=1;
 	DRIVE_A = 0;  
 	DRIVE_B = 1;
 
-	Delay(500); //cekamo jedno vreme da se odradi AD konverzija
+	Delay(500); // Wait for AD conversion to complete
 	
-	// ocitavamo y	
-	y_vrednost = temp1;// temp1 je vrednost koji nam daje AD konvertor na LEFT pinu	
+	// Read Y coordinate	
+	y_vrednost = temp1; // temp1 is the AD converter value on the LEFT pin	
 	
-//Ako ?elimo da nam X i Y koordinate budu kao rezolucija ekrana 128x64 treba skalirati vrednosti x_vrednost i y_vrednost tako da budu u opsegu od 0-128 odnosno 0-64
-//skaliranje x-koordinate
-
+    // To match X and Y coordinates to the 128x64 screen resolution, 
+    // scale x_vrednost and y_vrednost to the 0-128 and 0-64 ranges respectively.
+    // Scaling the X coordinate
     X=(x_vrednost-161)*0.03629;
 
+    // X = ((x_vrednost-AD_Xmin)/(AD_Xmax-AD_Xmin))*128;	
+    // AD_Xmin and AD_Xmax are the min and max ADC values for the touch panel.
 
-
-//X= ((x_vrednost-AD_Xmin)/(AD_Xmax-AD_Xmin))*128;	
-//vrednosti AD_Xmin i AD_Xmax su minimalne i maksimalne vrednosti koje daje AD konvertor za touch panel.
-
-
-//Skaliranje Y-koordinate
+    // Scaling the Y coordinate
 	Y= ((y_vrednost-500)*0.020725);
-
-//	Y= ((y_vrednost-AD_Ymin)/(AD_Ymax-AD_Ymin))*64;
+    // Y= ((y_vrednost-AD_Ymin)/(AD_Ymax-AD_Ymin))*64;
 }
 
 /***************************************************************************
-* Ime funkcije      : initUART1                                            *
-* Opis              : inicjalizuje RS232 komunikaciju s 9600bauda          * 
-* Parameteri        : Nema                                                 *
-* Povratna vrednost : Nema                                                 *
+* Function Name     : initUART1                                            *
+* Description       : Initializes RS232 communication at 9600 baud rate    * 
+* Parameters        : None                                                 *
+* Return Value      : None                                                 *
 ***************************************************************************/
-
 void initUART1(void)
 {
-    U1BRG=0x0040;//baud rate 9600
-    U1MODEbits.ALTIO = 0; //Koristimo UART1 sa osnovnim pinovima - RF2 i RF3
+    U1BRG=0x0040; // baud rate 9600
+    U1MODEbits.ALTIO = 0; // Use UART1 with default pins - RF2 and RF3
     IEC0bits.U1RXIE = 1;
     U1STA&=0xfffc;
     U1MODEbits.UARTEN=1;
@@ -135,47 +128,38 @@ void __attribute__((__interrupt__)) _U1RXInterrupt(void)
 
 void __attribute__((__interrupt__)) _ADCInterrupt(void) 
 {
-							
-
-										sirovi0=ADCBUF0; //TAC
-                                        sirovi1=ADCBUF1; //TAC
-                                        sirovi2=ADCBUF2; //LM34
-                                        sirovi3=ADCBUF3; //PIR
-                                        sirovi4=ADCBUF4; //FOTO
-                                        temp0=sirovi0;
-                                        temp1=sirovi1;
+    sirovi0=ADCBUF0; // Touch screen
+    sirovi1=ADCBUF1; // Touch screen
+    sirovi2=ADCBUF2; // LM34
+    sirovi3=ADCBUF3; // PIR
+    sirovi4=ADCBUF4; // FOTO (Photoresistor)
+    temp0=sirovi0;
+    temp1=sirovi1;
 										
-										
-
     IFS0bits.ADIF = 0;
-
 } 
 
 /*********************************************************************
-* Ime funkcije      : WriteUART1                            		 *
-* Opis              : Funkcija upisuje podatke u registar U1TXREG,   *
-*                     za slanje podataka    						 *
-* Parameteri        : unsigned int data-podatak koji zelimo poslati  *
-* Povratna vrednost : Nema                                           *
+* Function Name     : WriteUART1                            		 *
+* Description       : Writes data to the U1TXREG register            *
+*                     to transmit data    						     *
+* Parameters        : unsigned int data - data to be sent            *
+* Return Value      : None                                           *
 *********************************************************************/
-
 void WriteUART1(unsigned int data)
 {
-
-
 	while (U1STAbits.TRMT==0);
     if(U1MODEbits.PDSEL == 3)
         U1TXREG = data;
-        
     else 
         U1TXREG = data & 0xFF;
-        
 }
+
 /***********************************************************************
-* Ime funkcije      : WriteUART1dec2string                     		   *
-* Opis              : Funkcija salje 4-cifrene brojeve (cifru po cifru)*
-* Parameteri        : unsigned int data-podatak koji zelimo poslati    *
-* Povratna vrednost : Nema                                             *
+* Function Name     : WriteUART1dec2string                     		   *
+* Description       : Sends 4-digit numbers (digit by digit)           *
+* Parameters        : unsigned int data - data to be sent              *
+* Return Value      : None                                             *
 ************************************************************************/
 void WriteUART1dec2string(unsigned int data)
 {
@@ -193,87 +177,84 @@ void WriteUART1dec2string(unsigned int data)
 	WriteUART1(data+'0');
 }
 
-
 /*******************************************************************
-//funkcija za kasnjenje u milisekundama
+// Function for delay in milliseconds
 *********************************************************************/
-void Delay_ms (int vreme)//funkcija za kasnjenje u milisekundama
-	{
-		stoperica = 0;
-		while(stoperica < vreme);
-	}
-void Delay_ms3 (int vreme3)//funkcija za kasnjenje u milisekundama
-	{
-		stoperica3 = 0;
-		while(stoperica3 < vreme3);
-	}
-
-
-//PREKIDNA RUTINA TAJMERA 2 
-void __attribute__ ((__interrupt__)) _T2Interrupt(void) // svakih 1ms
+void Delay_ms (int vreme) 
 {
-
-		TMR2 =0;
-     ms=1;//fleg za milisekundu ili prekid;potrebno ga je samo resetovati u funkciji
-
-	brojac_ms++;//brojac milisekundi
-    stoperica++;//brojac za funkciju Delay_ms
-
-    if (brojac_ms==1000)//sek
-        {
-          brojac_ms=0;
-          sekund=1;//fleg za sekundu
-		 } 
-	IFS0bits.T2IF = 0; 
-       
-}
-//PREKIDNA RUTINA TAJMERA 3
-void __attribute__ ((__interrupt__)) _T3Interrupt(void) // svakih 1ms
-{
-
-		TMR3 =0;
-     ms3=1;//fleg za milisekundu ili prekid;potrebno ga je samo resetovati u funkciji
-
-	brojac_ms3++;//brojac milisekundi
-    stoperica3++;//brojac za funkciju Delay_ms
-
-    if (brojac_ms3==1000)//sek
-        {
-          brojac_ms3=0;
-          sekund3=1;//fleg za sekundu
-		 } 
-	IFS0bits.T3IF = 0; 
-       
+    stoperica = 0;
+    while(stoperica < vreme);
 }
 
-//Funkcija za pokretanje sistema - kada se sa terminala posalje ON, sistem se pokrece
+void Delay_ms3 (int vreme3) 
+{
+    stoperica3 = 0;
+    while(stoperica3 < vreme3);
+}
+
+// TIMER 2 INTERRUPT ROUTINE
+void __attribute__ ((__interrupt__)) _T2Interrupt(void) // Every 1ms
+{
+    TMR2 = 0;
+    ms=1; // millisecond or interrupt flag; only needs to be reset in the function
+
+    brojac_ms++; // millisecond counter
+    stoperica++; // counter for Delay_ms function
+
+    if (brojac_ms==1000) // 1 second reached
+    {
+        brojac_ms=0;
+        sekund=1; // second flag
+    } 
+    IFS0bits.T2IF = 0; 
+}
+
+// TIMER 3 INTERRUPT ROUTINE
+void __attribute__ ((__interrupt__)) _T3Interrupt(void) // Every 1ms
+{
+    TMR3 = 0;
+    ms3=1; // millisecond or interrupt flag; only needs to be reset in the function
+
+    brojac_ms3++; // millisecond counter
+    stoperica3++; // counter for Delay_ms function
+
+    if (brojac_ms3==1000) // 1 second reached
+    {
+        brojac_ms3=0;
+        sekund3=1; // second flag
+    } 
+    IFS0bits.T3IF = 0; 
+}
+
+// Function to start the system - when "ON" is sent from the terminal, system starts
 void start(){
     
     if(buf[0] == 'O' && buf[1] == 'N') {
             //GLCD_ClrScr();
             flag=1;
             GoToXY(1,3);
-            GLCD_Printf("Sistem je upaljen");
+            GLCD_Printf("System is ON");
             Delay_ms3(500);
             //GLCD_ClrScr();
         } 
 }
 
-//Funkcija koja, kada je pritisnut ekran, omogucava da se proveri da li je RD8 upaljena ili ne
+// Function that allows checking if the RD8 LED is ON when the screen is pressed
 void pritisnut_ekran(){
     
     if(X >0  && X < 128 && Y > 0 && Y <64){         
             GLCD_ClrScr();
             GoToXY(1,3);
-            GLCD_Printf("Ekran je pritisnut");
+            GLCD_Printf("Screen pressed");
             GoToXY(1,4);
-            GLCD_Printf("Proveri da li je dioda RD8 upaljena");//Posto LM35 radi okej u slucaju kada motor ne radi, vuce ad konverzija previse,    
-            Delay_ms(5000);                                    //na ovaj nacin zaustavljamo sve i proveravamo da li se senzor zagrejao do temperature od 30
-            GLCD_ClrScr();                                     //stepeni, ako jeste, pali se lampica RD8 (5/4095=0,001220703125; 0,001220703125/0,01=0,1220703125;
-    }                                                          //                                         30/0,1220703125=245,76)
+            GLCD_Printf("Check if RD8 LED is ON"); // Since LM35 works fine when motor is off, but ADC draws   
+            Delay_ms(5000);                        // too much load otherwise, this stops everything to check 
+            GLCD_ClrScr();                         // if sensor reached 30 deg. If so, RD8 LED turns on.
+    }                                              // (5/4095=0.001220703125; 0.001220703125/0.01=0.1220703125;
+                                                   // 30/0.1220703125=245.76)
 }
 
-//Funkcija koja ispisuje string na terminalu
+// Function that prints a string to the serial terminal
 void RS232_putst(register const char *str)
 {
     while((*str!=0))
@@ -285,16 +266,16 @@ void RS232_putst(register const char *str)
     }
 }
 
-//Ispisivanje vrednosti AD konverzije sa LM35 preko UART-a na terminalu
+// Print LM35 ADC values via UART to the terminal
 void ispis_temp(){  
     
         RS232_putst("TEMP ");
 		WriteUART1dec2string(sirovi2);
         for(broj2=0;broj2<100;broj2++);
-		WriteUART1(13);//enter
+		WriteUART1(13); // enter
 }
 
-//Funkcija koja pali diodu RD8 ukoliko je postignuta zeljena temperatura na LM35
+// Function that turns on the RD8 LED if the desired temperature is reached on LM35
 void pali_rd8(){
     
     if(sirovi2>250){
@@ -305,30 +286,29 @@ void pali_rd8(){
         }
 }
 
- //Ispisivanje vrednosti AD konverzije sa fotootpornika preko UART-a na terminalu
+// Print photoresistor ADC values via UART to the terminal
 void ispis_foto(){
     
         RS232_putst("FOTO ");
 		WriteUART1dec2string(sirovi4);
         for(broj2=0;broj2<100;broj2++);
-		WriteUART1(13);//enter
+		WriteUART1(13); // enter
 }
 
-
-//Ispisivanje vrednosti AD konverzije sa PIR senzora preko UART-a na terminalu
+// Print PIR sensor ADC values via UART to the terminal
 void ispis_pir(){
     
         RS232_putst("PIR ");
 		WriteUART1dec2string(sirovi3);
 		for(broj2=0;broj2<100;broj2++);
-		WriteUART1(13);//enter
+		WriteUART1(13); // enter
 }
 
-//Funkcija koja simulira parking senzore - kada se detektuje pokret preko PIR senzora, pali se piezo buzzer
+// Simulates parking sensors - when movement is detected via PIR, piezo buzzer is activated
 void parking_senzori(){
     
         if(sirovi3>2000){
-                //Piezo Buzzer
+                // Piezo Buzzer
                 LATAbits.LATA11=1;
                 Delay_ms(10);
                 LATAbits.LATA11=0;
@@ -336,16 +316,15 @@ void parking_senzori(){
             } 
 }
 
-
-//Simuliranje rada brisaca na autu preko servo motora SG90, u zavisnosti od vrednosti konverzije sa fotootpornika,
-//pale se brisaci 
+// Simulates car wipers using the SG90 servo motor. 
+// Wipers are activated based on photoresistor ADC values.
 void brisaci(){
     
     if(sirovi4<=150){
                 
             GLCD_ClrScr();
             GoToXY(0,0);
-            GLCD_Printf("Kisa pljusti");
+            GLCD_Printf("Heavy rain");
             
             for(broj2=0; broj2<25; broj2++){
                 LATDbits.LATD9=1;
@@ -365,14 +344,14 @@ void brisaci(){
     if(sirovi4<=1800 && sirovi4>150){
             //GLCD_ClrScr();
             GoToXY(0,0);
-            GLCD_Printf("Sunce sija");
+            GLCD_Printf("Sunny");
         }
     
     if(sirovi4>1800){
                  
             GLCD_ClrScr(); 
             GoToXY(0,0);
-            GLCD_Printf("Kisa sipi");
+            GLCD_Printf("Light rain");
             
             for(broj2=0; broj2<60; broj2++){
                 LATDbits.LATD9=1;
@@ -395,25 +374,25 @@ int main(int argc, char** argv) {
     for(broj1=0;broj1<10000;broj1++);
     for(broj=0;broj<60000;broj++);
 
-        flag = 0; //Inicijalna vrednost flag-a
-        Init_T2(); //Inicijalizacija tajmera 2
-        Init_T3(); //Inicijalizacija tajmera 3
+        flag = 0; // Initial flag value
+        Init_T2(); // Timer 2 initialization
+        Init_T3(); // Timer 3 initialization
         
-		initUART1();//inicijalizacija UART-a
- 		ADCinit();//inicijalizacija AD konvertora
-        ADCON1bits.ADON=1;//pocetak Ad konverzije 
+		initUART1(); // UART initialization
+ 		ADCinit(); // ADC initialization
+        ADCON1bits.ADON=1; // Start AD conversion
         
-        //Inicijalizacija GLCD-a
+        // GLCD Initialization
         ConfigureAllPins();
         GLCD_LcdInit();
         GLCD_ClrScr();
         
-        //Touch screen
+        // Touch screen setup
         ConfigureTSPins();
         
-        TRISDbits.TRISD9=0; //Izlaz za servo
-        TRISDbits.TRISD8=0; //Izlaz za LM35(pali diodu RD8 kada detektuje odredjenu temperaturu)
-        TRISAbits.TRISA11=0;//Izlaz za buzzer
+        TRISDbits.TRISD9=0; // Output for servo
+        TRISDbits.TRISD8=0; // Output for LM35 (turns on RD8 LED when specific temp is detected)
+        TRISAbits.TRISA11=0; // Output for buzzer
         
         
 	while(1)
@@ -430,11 +409,9 @@ int main(int argc, char** argv) {
             ispis_pir();
             parking_senzori();
             brisaci();
-        
         }
       
-                  
-		}//while  
+		} // end while  
 
     return (EXIT_SUCCESS);
-}
+} // end main
